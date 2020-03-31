69355568f0fde1c82295678f4c5bd39e806e258e
Crowdbotics <team@crowdbotics.com>
2020-03-31 18:07:33 +0000

    Update models

--- backend/closeout_helper_15268/wsgi.py
+++ backend/closeout_helper_15268/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'closeout_helper_15268.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "closeout_helper_15268.settings")
 
 application = get_wsgi_application()
